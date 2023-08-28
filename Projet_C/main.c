d6f8b0009b46aa06ef75990371279c1b19a1f037
Romain Scarato <romainscarato@mgail.com>
2023-08-28 17:53:20 +0200

    ajout du choix de chiffrement et gestion des choix

--- Projet_C/main.c
+++ Projet_C/main.c
@@ -3,12 +3,48 @@
 int main()
 {
     struct Option_gene general;
-
     strcpy(general.dest, "dest.crt");
     strcpy(general.source, "source.txt");
 
-    menu(general);
+    bool exit = false;
+    int choix;
 
+    while (!exit)
+    {
+        menu(general);
+        scanf("Quel est votre choix ?  %d",&choix);
+        switch (choix)
+        {
+        case 1:
+            printf("\nChangement du peroquet.\n");
+            scanf("Entrez le nouveau peroquet : %s",&general.passfrase);
+            break;
+        
+        case 2:
+            printf("\nChangement du fichier source.\n");
+            scanf("Entrez le chemin du fichier source : %s",&general.passfrase);
+            break;
+
+        case 3:
+            printf("\nChangement du fichier destination.\n");
+            scanf("Entrez le chemin du fichier de destination : %s",&general.passfrase);
+            break;
+
+        case 4:
+            printf("\nChiffrement du message en cour.\n");
+            exit = true;
+            break;
+
+        case 5:
+            printf("\nAurevoir.\n");
+            exit = true;
+            break;
+        
+        default:
+            printf("Choix incorrect");
+            break;
+        }
+    }
 
     return 0;
 }
--- Projet_C/main.h
+++ Projet_C/main.h
@@ -1,14 +1,23 @@
 #ifndef MAIN_INCLUDED
 #define MAIN_INCLUDED
 
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> 
+
+
+#include "menu.c"
+#include "chifrement.c"
+
+
 
 struct Option_gene
 {
     char passfrase[10 + 1];
-    char source[11 + 1];
-    char dest[11 + 1];
+    char source[50 + 1];
+    char dest[50 + 1];
 };
 
 
